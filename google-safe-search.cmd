93e10f1f2f68db0d174902cfd3774d8f3790df7b
Amirhossein arabahmadi <121059574+amirdecoder@users.noreply.github.com>
2023-08-25 05:51:44 +0330

    Add files via upload

---
+++ google-safe-search.cmd
@@ -0,0 +1,14 @@
+::@HackLSD
+@echo off
+TITLE Amirdecoder
+COLOR 0A
+ECHO Start
+ECHO.
+ECHO Developed and promoted by amirdecoder
+ECHO.
+ECHO Telegram Channel :
+echo @HackLSD
+echo 142.250.180.142 google.com >> "C:\Windows\System32\drivers\etc\hosts"
+echo 142.250.180.142 www.google.com >> "C:\Windows\System32\drivers\etc\hosts"
+
+pause
